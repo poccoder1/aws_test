0bf4a553c5ec2c669ef5cad6a47fa3231aea11b4
poccoder1 <poc.code.test@gmail.com>
2023-02-28 13:00:01 +0800

    test code for glue validation

--- glue_poc/Glue_ETL.py
+++ glue_poc/Glue_ETL.py
@@ -1,3 +1,6 @@
+# Define the mapping as a dictionary with dynamic values
+mapping = {'name': 'customer_name', 'age': 'customer_age', 'gender': 'customer_gender'}
+
 # Add hardcoded currency column with header set to 'currency'
 transformed_df = transformed_dyf.toDF()
 transformed_df = transformed_df \
@@ -11,3 +14,16 @@ rows = transformed_df \
     .map(lambda x: x[0]) \
     .collect()
 new_df = spark.createDataFrame(rows, transformed_df.schema)
+
+# Rename columns dynamically based on a dictionary of column mappings
+for current_name, new_name in mapping.items():
+    new_df = new_df.withColumnRenamed(current_name, new_name)
+
+# Write DataFrame to a single file
+new_df \
+    .coalesce(1) \
+    .write \
+    .format('parquet') \
+    .mode('overwrite') \
+    .option('compression', 'snappy') \
+    .save('s3://my-bucket/output')
