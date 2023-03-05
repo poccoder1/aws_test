ce1d547c27a981732684239cddb9ed0f31a6a1d2
poccoder1 <poc.code.test@gmail.com>
2023-03-05 13:47:20 +0800

    test code for glue validation

--- glue_poc/Glue_ETL.py
+++ glue_poc/Glue_ETL.py
@@ -1,21 +1,7 @@
-from awsglue.context import GlueContext
-from awsglue.dynamicframe import DynamicFrame
+# Define logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-# Initialize GlueContext
-glueContext = GlueContext(SparkContext.getOrCreate())
-
-# Define source path and format
-src_path = "s3://my-bucket/my-file.csv"
-src_format = "csv"
-
-# Read data from source file into a DynamicFrame
-dynamic_frame = glueContext.create_dynamic_frame_from_options(
-    connection_type="s3",
-    format=src_format,
-    connection_options={"paths": [src_path]},
-    transformation_ctx="dynamic_frame"
-)
-
-# Print schema and show data
-print(dynamic_frame.schema())
-dynamic_frame.show()
+# Redirect printSchema() output to logger
+logger.info("Schema:")
+dynamic_frame.printSchema(log_level=logging.INFO)
