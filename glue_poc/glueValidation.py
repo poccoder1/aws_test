dcf158a6c5c112746a7a95e0f8f7c22c5866670f
poccoder1 <poc.code.test@gmail.com>
2023-03-14 09:07:31 +0800

    test code for glue validation

--- glue_poc/glueValidation.py
+++ glue_poc/glueValidation.py
@@ -1,22 +1,30 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
 
-def parse_xml_to_df(xml_str):
-    root = ET.fromstring(xml_str)
-    rows = []
-    columns = []
+def parse_xml_to_df(xml_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    df = pd.DataFrame(columns=[child.tag for child in root[0]])
+
     for child in root:
-        if not columns:
-            columns.append('Index')
-            for element in child.iter():
-                columns.append(element.tag)
-            columns = [col.replace('{http://www.fixprotocol.org/FIXML-5-0-SP2}', '') for col in columns]
-            columns = [col.replace('_', ' ') for col in columns]
-        row = [str(i) for i in range(len(rows)+1)]
-        for element in child.iter():
-            if element.text:
-                row.append(element.text)
+        row = {}
+        for element in child:
+            if len(element) == 0:
+                row[element.tag] = element.text
             else:
-                row.append('')
-        rows.append(row)
-    return pd.DataFrame(rows, columns=columns)
+                row[element.tag] = json.dumps(xml_to_dict(element))
+        df = df.append(row, ignore_index=True)
+
+    return df
+
+def xml_to_dict(xml_element):
+    if len(xml_element) == 0:
+        return xml_element.text
+    else:
+        return {child.tag: xml_to_dict(child) for child in xml_element}
+
+# Example usage:
+xml_file = "example.xml"
+
+df = parse_xml_to_df(xml_file)
+print(df)
