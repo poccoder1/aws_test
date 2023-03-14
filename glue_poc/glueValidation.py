f23ee5c4746da559c7e18970e7226f31aec21d0b
poccoder1 <poc.code.test@gmail.com>
2023-03-14 09:19:09 +0800

    test code for glue validation

--- glue_poc/glueValidation.py
+++ glue_poc/glueValidation.py
@@ -1,32 +1,33 @@
-import xml.etree.ElementTree as ET
+import xmltodict
 import pandas as pd
-import json
 
-def parse_xml_to_df(xml_file):
-    root = ET.parse(xml_file).getroot()
-    df = pd.DataFrame(columns=[child.tag for child in root[0]])
+def xml_to_dataframe(file_path):
+    with open(file_path) as f:
+        xml_data = f.read()
 
-    for child in root:
-        row = {}
-        for element in child:
-            if len(element) == 0:
-                row[element.tag] = element.text
-            else:
-                row[element.tag] = json.dumps(xml_to_dict(element))
-        df = df.append(row, ignore_index=True)
+    xml_dict = xmltodict.parse(xml_data)
+    asgn_rpt_list = xml_dict['FIXML']['AsgnRpt']
+    if isinstance(asgn_rpt_list, dict):
+        asgn_rpt_list = [asgn_rpt_list]
 
-    df.insert(0, 'index', range(1, len(df) + 1))
-    df.set_index('index', inplace=True)
+    df_list = []
+    for asgn_rpt in asgn_rpt_list:
+        row_dict = {'RptID': asgn_rpt['@RptID'], 'BizDt': asgn_rpt['@BizDt'], 'Ccy': asgn_rpt['@Ccy'], 'SetSesID': asgn_rpt['@SetSesID'], 'UndSetPx': asgn_rpt['@UndSetPx'], 'AsgnMeth': asgn_rpt['@AsgnMeth']}
+        pty_list = asgn_rpt.get('Pty', [])
+        if isinstance(pty_list, dict):
+            pty_list = [pty_list]
+        row_dict.update({'Pty{}_ID'.format(i+1): pty['@ID'] for i, pty in enumerate(pty_list)})
+        row_dict.update({'Pty{}_R'.format(i+1): pty['@R'] for i, pty in enumerate(pty_list)})
+        instrmt = asgn_rpt.get('Instrmt', {})
+        row_dict.update({'Instrmt_ID': instrmt.get('@ID', ''), 'SecTyp': instrmt.get('@SecTyp', ''), 'MMY': instrmt.get('@MMY', ''), 'Exch': instrmt.get('@Exch', ''), 'StrkPx': instrmt.get('@StrkPx', ''), 'PutCall': instrmt.get('@PutCall', ''), 'Fctr': instrmt.get('@Fctr', ''), 'PCFctr': instrmt.get('@PCFctr', '')})
+        undly = asgn_rpt.get('Undly', {})
+        row_dict.update({'Undly_ID': undly.get('@ID', ''), 'Undly_SecTyp': undly.get('@SecTyp', ''), 'Undly_MMY': undly.get('@MMY', ''), 'Undly_Exch': undly.get('@Exch', '')})
+        qty_list = asgn_rpt.get('Qty', [])
+        if isinstance(qty_list, dict):
+            qty_list = [qty_list]
+        row_dict.update({'Qty{}_Short'.format(i+1): qty['@Short'] for i, qty in enumerate(qty_list)})
+        row_dict.update({'Qty{}_Typ'.format(i+1): qty['@Typ'] for i, qty in enumerate(qty_list)})
+        df_list.append(row_dict)
+
+    df = pd.DataFrame(df_list)
     return df
-
-def xml_to_dict(xml_element):
-    if len(xml_element) == 0:
-        return xml_element.text
-    else:
-        return {child.tag: xml_to_dict(child) for child in xml_element}
-
-# Example usage:
-xml_file = "example.xml"
-
-df = parse_xml_to_df(xml_file)
-print(df)
