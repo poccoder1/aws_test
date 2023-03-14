0af061307726d97d163dbcdc083d4079a1111eac
poccoder1 <poc.code.test@gmail.com>
2023-03-14 08:56:30 +0800

    test code for glue validation

--- glue_poc/glueValidation.py
+++ glue_poc/glueValidation.py
@@ -1,49 +1,35 @@
 import xml.etree.ElementTree as ET
-import csv
-import json
-import logging
+import pandas as pd
 
-logging.basicConfig(filename='xml_parser.log', level=logging.ERROR)
+# Parse XML file and get root element
+tree = ET.parse('filename.xml')
+root = tree.getroot()
 
-def parse_xml_to_csv(xml_file_path, csv_file_path):
-    try:
-        # Open the XML file
-        tree = ET.parse(xml_file_path)
-        root = tree.getroot()
+# Create list to hold dictionaries of element data
+elements_list = []
 
-        # Open the CSV file for writing
-        with open(csv_file_path, 'w', newline='') as csv_file:
-            writer = csv.writer(csv_file)
+# Iterate over all child elements of root
+for elem in root.iter():
+    # Get element tag and attributes
+    tag = elem.tag
+    attributes = elem.attrib
 
-            # Write the header row
-            header_row = []
-            for child in root[0]:
-                header_row.append(child.tag)
-            writer.writerow(header_row)
+    # Create dictionary to hold element data
+    element_dict = {'tag': tag}
 
-            # Write each row of data
-            for item in root:
-                data_row = []
-                for child in item:
-                    if len(child) > 0:
-                        # If the child has nested data, convert it to JSON format
-                        data_row.append(json.dumps(xml_to_dict(child)))
-                    else:
-                        data_row.append(child.text)
-                writer.writerow(data_row)
-    except Exception as e:
-        logging.exception(f'Error while parsing {xml_file_path}: {e}')
+    # Add attributes to dictionary
+    if attributes:
+        element_dict.update(attributes)
 
-def xml_to_dict(xml):
-    # Convert an XML element to a dictionary
-    if len(xml) == 0:
-        return xml.text
-    result = {}
-    for child in xml:
-        if child.tag in result:
-            if type(result[child.tag]) is not list:
-                result[child.tag] = [result[child.tag]]
-            result[child.tag].append(xml_to_dict(child))
-        else:
-            result[child.tag] = xml_to_dict(child)
-    return result
+    # Add text of element to dictionary
+    if elem.text and elem.text.strip():
+        element_dict['text'] = elem.text.strip()
+
+    # Append dictionary to list
+    elements_list.append(element_dict)
+
+# Convert list of dictionaries to pandas DataFrame
+df = pd.DataFrame(elements_list)
+
+# Output DataFrame as table
+print(df.to_string(index=False))
