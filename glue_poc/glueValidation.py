1e7ae724b47f537ce8fd55de5237c62d82745c45
poccoder1 <poc.code.test@gmail.com>
2023-03-14 09:14:45 +0800

    test code for glue validation

--- glue_poc/glueValidation.py
+++ glue_poc/glueValidation.py
@@ -1,5 +1,9 @@
-def parse_xml_to_df(xml_string):
-    root = ET.fromstring(xml_string)
+import xml.etree.ElementTree as ET
+import pandas as pd
+import json
+
+def parse_xml_to_df(xml_file):
+    root = ET.parse(xml_file).getroot()
     df = pd.DataFrame(columns=[child.tag for child in root[0]])
 
     for child in root:
@@ -13,3 +17,15 @@ def parse_xml_to_df(xml_string):
 
     df.set_index('name', inplace=True)
     return df
+
+def xml_to_dict(xml_element):
+    if len(xml_element) == 0:
+        return xml_element.text
+    else:
+        return {child.tag: xml_to_dict(child) for child in xml_element}
+
+# Example usage:
+xml_file = "example.xml"
+
+df = parse_xml_to_df(xml_file)
+print(df)
