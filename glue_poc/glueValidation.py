2c50667db3bcbeba14d1c3076fa80f0f0cac699f
poccoder1 <poc.code.test@gmail.com>
2023-03-17 21:14:58 +0800

    test code for glue validation

--- glue_poc/glueValidation.py
+++ glue_poc/glueValidation.py
@@ -1,10 +1,1 @@
- convert dynamic frame to data frame
-df = dyf.toDF()
-
-# capture output of df.show() using StringIO
-output = StringIO()
-df.show(10, False, True, False, False, False, truncate=False, file=output)
-output_string = output.getvalue()
-
-# log the captured output using logger.info()
-logger.info("Printing DataFrame:\n" + output_string)
+output = "\n".join([str(row) for row in df.limit(10).collect()])
