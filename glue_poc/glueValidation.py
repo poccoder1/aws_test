0026523851a06ffdc4604b41b7eba1e15ad3c736
poccoder1 <poc.code.test@gmail.com>
2023-03-20 21:54:52 +0800

    test code for glue validation

--- glue_poc/glueValidation.py
+++ glue_poc/glueValidation.py
@@ -1,30 +1,27 @@
-from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
 from pydeequ.checks import *
 from pydeequ.verification import *
 
-# create a SparkSession
-spark = SparkSession.builder.appName("DataQualityCheck").getOrCreate()
+checks = [
+    Analysis(
+        Column("col1"),
+        MaximumLength(len_=10),
+        "col1 has maximum length of 10"
+    ),
+    Analysis(
+        Column("col2"),
+        IsEqualTo("constant_value"),
+        "col2 is equals to some constant value"
+    ),
+    Analysis(
+        Column("col3"),
+        IsNotNull(),
+        "col3 is not null"
+    ),
+]
 
-# create a sample DataFrame
-data = [("foo", "bar", 1),
-        ("foo", "bar", 2),
-        ("foo", "baz", 3),
-        ("foo", "bar", None),
-        ("foo", "bar", 5),
-        ("foo", "bar", 6)]
-
-df = spark.createDataFrame(data, ["col1", "col2", "col3"])
-
-# define the constraints
-constraints = ConstraintSet(
-    [SizeConstraint(lambda x: x == 6),
-     ColumnConstraint("col1", "maximum_length", lambda x: x <= 10),
-     ColumnConstraint("col2", "isEqualTo", "bar"),
-     ColumnConstraint("col3", "isNotNull")])
-
-# run the verification
-result = VerificationSuite(spark).onData(df).addChecks([constraints]).run()
-
-# print the verification results
-for check_result in result.checkResults:
-    print(check_result)
+# Run the checks
+results = VerificationSuite(spark) \
+    .onData(df) \
+    .addChecks(checks) \
+    .run()
