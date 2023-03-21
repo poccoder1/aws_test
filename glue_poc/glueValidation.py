41dcea871e1db7287f6e8476a9e8b99057eb4e42
poccoder1 <poc.code.test@gmail.com>
2023-03-21 12:06:56 +0800

    test code for glue validation

--- glue_poc/glueValidation.py
+++ glue_poc/glueValidation.py
@@ -1,9 +1,28 @@
-spark-submit --jars /path/to/deequ-x.x.x.jar your_script.py
+from pyspark.sql import SparkSession
+from pydeequ.checks import *
+from pydeequ.verification import *
 
-https://mvnrepository.com/artifact/com.amazon.deequ/deequ
+# create a SparkSession
+spark = SparkSession.builder.appName("Data Quality Check").getOrCreate()
 
-<dependency>
-<groupId>com.amazon.deequ</groupId>
-<artifactId>deequ</artifactId>
-<version>2.0.0</version>
-</dependency>
+# read the input data into a dataframe
+df = spark.read.format("csv").option("header", True).load("path/to/your/data.csv")
+
+# create the checks
+checks = Check(
+    check_level=CheckLevel.Warning,
+    checks=[
+        Check(spark, CheckLevel.Warning, "Maximum length check for all columns", [
+            SizeConstraint(column, ">", 0) for column in df.columns
+        ]),
+        Check(spark, CheckLevel.Warning, "Null check for all columns", [
+            CompletenessConstraint(column) for column in df.columns
+        ])
+    ],
+)
+
+# run the checks and get the results
+result = VerificationSuite(spark).onData(df).addChecks(checks).run()
+
+# print the results
+print(result.checkResults)
