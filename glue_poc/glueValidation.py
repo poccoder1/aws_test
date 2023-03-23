2256a32877f4460c3d224aab805a8fc4ef7fdd91
poccoder1 <poc.code.test@gmail.com>
2023-03-23 16:08:00 +0800

    test code for glue validation

--- glue_poc/glueValidation.py
+++ glue_poc/glueValidation.py
@@ -1,45 +1,46 @@
-from pyspark.sql import SparkSession
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.dynamicframe import DynamicFrame
 from pyspark.sql.functions import explode
-from pyspark.sql.functions import from_unixtime
+from pyspark.sql.functions import col
+from pyspark.sql.functions import lit
+from pyspark.sql.functions import udf
+from pyspark.sql.types import StringType
 
-# create SparkSession
-spark = SparkSession.builder.appName("XMLReader").getOrCreate()
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 
-# read XML file
-xml_df = spark.read \
-    .format("com.databricks.spark.xml") \
-    .option("rowTag", "AsgnRpt") \
-    .load("path/to/xml/file.xml")
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = glueContext.Job(args['JOB_NAME'])
 
-# flatten all nested tags
-flat_df = xml_df.selectExpr("*", "explode(PTY) as PTY_expanded", "explode(Qty) as Qty_expanded") \
-    .drop("PTY", "Qty") \
-    .selectExpr("*", "PTY_expanded._id as PTY_ID", "PTY_expanded._r as PTY_R", "Qty_expanded._short as Qty_Short", "Qty_expanded._typ as Qty_Typ") \
-    .drop("PTY_expanded", "Qty_expanded") \
-    .withColumn("RptID", xml_df["_RptID"]) \
-    .withColumn("BizDt", from_unixtime(xml_df["_BizDt"], "yyyy-MM-dd")) \
-    .withColumn("SetSesID", xml_df["_SetSesID"]) \
-    .withColumn("UndSetPx", xml_df["_UndSetPx"]) \
-    .withColumn("AsgnMeth", xml_df["_AsgnMeth"]) \
-    .withColumnRenamed("_Ccy", "Ccy") \
-    .withColumn("Sub_Id", xml_df["PTY.Sub._Id"]) \
-    .withColumn("Sub_Type", xml_df["PTY.Sub._type"]) \
-    .withColumn("Instrmt_ID", xml_df["Instrmt._ID"]) \
-    .withColumn("SecType", xml_df["Instrmt._SecType"]) \
-    .withColumn("MMY", xml_df["Instrmt._MMY"]) \
-    .withColumn("Exch", xml_df["Instrmt._Exch"]) \
-    .withColumn("StrKPx", xml_df["Instrmt._StrKPx"]) \
-    .withColumn("PutCall", xml_df["Instrmt._PutCall"]) \
-    .withColumn("Fctr", xml_df["Instrmt._Fctr"]) \
-    .withColumn("PCFctr", xml_df["Instrmt._PCFctr"]) \
-    .withColumn("Undly_ID", xml_df["Undly._ID"]) \
-    .withColumn("SecTyp", xml_df["Undly._SecTyp"]) \
-    .withColumn("Undly_MMY", xml_df["Undly._MMY"]) \
-    .withColumn("Undly_Exch", xml_df["Undly._Exch"]) \
-    .drop("_RptID", "_BizDt", "_SetSesID", "_UndSetPx", "_AsgnMeth", "PTY.Sub._Id", "PTY.Sub._type", "Instrmt._ID", "Instrmt._SecType", "Instrmt._MMY", "Instrmt._Exch", "Instrmt._StrKPx", "Instrmt._PutCall", "Instrmt._Fctr", "Instrmt._PCFctr", "Undly._ID", "Undly._SecTyp", "Undly._MMY", "Undly._Exch")
+## Read XML File
+xml_source = "s3://path/to/xml/file"
+xml_df = spark.read.format("com.databricks.spark.xml").option("rowTag", "FIXML").load(xml_source)
 
-# show flattened data
-flat_df.show()
+## Create a function to flatten the nested XML tags
+def flatten_xml(nested):
+    flat_dict = {}
+    for key, value in nested.items():
+        if isinstance(value, dict):
+            flat_dict.update(flatten_xml(value))
+        else:
+            flat_dict[key] = value
+    return flat_dict
 
-###
+flatten_udf = udf(flatten_xml, StringType())
 
+## Apply the flatten function to each row of the DataFrame
+flat_df = xml_df.select(explode(col("AsgnRpt")).alias("AsgnRpt")) \
+    .withColumn("flat_AsgnRpt", flatten_udf(col("AsgnRpt"))) \
+    .select([col("flat_AsgnRpt." + c).alias(c) for c in flat_df.columns])
+
+## Write flattened DataFrame to output file
+output_path = "s3://path/to/output/file"
+flat_df.write.format("csv").option("header", "true").mode("overwrite").save(output_path)
+
+job.commit()
