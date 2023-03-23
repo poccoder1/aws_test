e8158b7add5008eb52cab324538b83e59a109e5c
poccoder1 <poc.code.test@gmail.com>
2023-03-23 17:23:24 +0800

    test code for glue validation

--- glue_poc/glueValidation.py
+++ glue_poc/glueValidation.py
@@ -7,16 +7,20 @@ tree = ET.parse(xml_file)
 root = tree.getroot()
 
 # Define a function to generate the schema for a given XML element
-def generate_schema(elem):
+def generate_schema(elem, unique_tags):
     schema = []
     for child in elem:
+        if child.tag in unique_tags:
+            continue
+        else:
+            unique_tags.add(child.tag)
         if child.tag not in [e.tag for e in elem.findall("*")]:
             # Add a new field to the schema for scalar values
             schema.append(StructField("@" + child.tag, StringType()))
         else:
             # Add a new field to the schema for nested structures
-            schema.append(StructField(child.tag, generate_schema(child), True))
+            schema.append(StructField(child.tag, generate_schema(child, unique_tags), True))
     return StructType(schema)
 
 # Generate the input schema dynamically
-input_schema = generate_schema(root)
+input_schema = generate_schema(root, set())
