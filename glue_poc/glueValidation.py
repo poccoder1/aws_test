8b65e9a2d420fe41af3261df075799b4a1eaa1bc
poccoder1 <poc.code.test@gmail.com>
2023-03-23 16:26:34 +0800

    test code for glue validation

--- glue_poc/glueValidation.py
+++ glue_poc/glueValidation.py
@@ -1,46 +1,142 @@
-import sys
-from awsglue.transforms import *
+from awsglue.dynamicframe import DynamicFrame
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
-from awsglue.context import GlueContext
-from awsglue.dynamicframe import DynamicFrame
 from pyspark.sql.functions import explode
-from pyspark.sql.functions import col
-from pyspark.sql.functions import lit
-from pyspark.sql.functions import udf
-from pyspark.sql.types import StringType
+from pyspark.sql.session import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType
 
-## @params: [JOB_NAME]
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+# Get the Glue job arguments
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'input_path', 'output_path'])
 
+# Create a Spark context and session
 sc = SparkContext()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
-job = glueContext.Job(args['JOB_NAME'])
+spark = SparkSession(sc)
 
-## Read XML File
-xml_source = "s3://path/to/xml/file"
-xml_df = spark.read.format("com.databricks.spark.xml").option("rowTag", "FIXML").load(xml_source)
+# Define the input schema
+input_schema = StructType([
+    StructField("AsgnRpt", StructType([
+        StructField("@RptID", StringType()),
+        StructField("@BizDt", StringType()),
+        StructField("@Ccy", StringType()),
+        StructField("@SetSesID", StringType()),
+        StructField("@UndSetPx", StringType()),
+        StructField("@AsgnMeth", StringType()),
+        StructField("PTY", StructType([
+            StructField("@ID", StringType()),
+            StructField("@R", StringType()),
+            StructField("Sub", StructType([
+                StructField("@Id", StringType()),
+                StructField("@type", StringType())
+            ]))
+        ], True),
+                    StructField("Instrmt", StructType([
+                        StructField("@ID", StringType()),
+                        StructField("@SecType", StringType()),
+                        StructField("@MMY", StringType()),
+                        StructField("@Exch", StringType()),
+                        StructField("@StrKPx", StringType()),
+                        StructField("@PutCall", StringType()),
+                        StructField("@Fctr", StringType()),
+                        StructField("@PCFctr", StringType())
+                    ], True),
+                                StructField("Undly", StructType([
+                                    StructField("@ID", StringType()),
+                                    StructField("@SecTyp", StringType()),
+                                    StructField("@MMY", StringType()),
+                                    StructField("@Exch", StringType())
+                                ], True),
+                                            StructField("Qty", StructType([
+                                                StructField("@Short", StringType()),
+                                                StructField("@Typ", StringType())
+                                            ], True))
+    ], True))
+])
 
-## Create a function to flatten the nested XML tags
-def flatten_xml(nested):
-    flat_dict = {}
-    for key, value in nested.items():
-        if isinstance(value, dict):
-            flat_dict.update(flatten_xml(value))
-        else:
-            flat_dict[key] = value
-    return flat_dict
+# Read the input XML file
+input_df = spark.read \
+    .format("com.databricks.spark.xml") \
+    .option("rowTag", "AsgnRpt") \
+    .option("rootTag", "FIXML") \
+    .schema(input_schema) \
+    .load(args['input_path'])
 
-flatten_udf = udf(flatten_xml, StringType())
+# Flatten the input dataframe
+output_df = input_df.selectExpr(
+    "AsgnRpt.@RptID AS AsgnRpt_RptID",
+    "AsgnRpt.@BizDt AS AsgnRpt_BizDt",
+    "AsgnRpt.@Ccy AS AsgnRpt_Ccy",
+    "AsgnRpt.@SetSesID AS AsgnRpt_SetSesID",
+    "AsgnRpt.@UndSetPx AS AsgnRpt_UndSetPx",
+    "AsgnRpt.@AsgnMeth AS AsgnRpt_AsgnMeth",
+    "explode(PTY) AS PTY",
+    "Instrmt.@ID AS AsgnRpt_Instrmt_ID",
+    "Instrmt.@SecType AS AsgnRpt_Instrmt_SecType",
+    "Instrmt.@MMY AS AsgnRpt_Instrmt_MMY",
+    "Instrmt.@Exch AS AsgnRpt_Instrmt_Exch",
+    "Instrmt.@StrKPx AS AsgnRpt_In")
 
-## Apply the flatten function to each row of the DataFrame
-flat_df = xml_df.select(explode(col("AsgnRpt")).alias("AsgnRpt")) \
-    .withColumn("flat_AsgnRpt", flatten_udf(col("AsgnRpt"))) \
-    .select([col("flat_AsgnRpt." + c).alias(c) for c in flat_df.columns])
 
-## Write flattened DataFrame to output file
-output_path = "s3://path/to/output/file"
-flat_df.write.format("csv").option("header", "true").mode("overwrite").save(output_path)
+#===========
 
-job.commit()
+from pyspark.sql.functions import explode
+
+# Flatten the nested XML structure using spark-xml
+df = spark.read.format("xml") \
+    .option("rowTag", "AsgnRpt") \
+    .load("path/to/xml/file")
+
+# Create a flattened dataframe by exploding each nested array of structs
+flat_df = df.selectExpr(
+    "RptID",
+    "BizDt",
+    "Ccy",
+    "SetSesID",
+    "UndSetPx",
+    "AsgnMeth",
+    "explode(PTY) as PTY",
+    "Instrmt.ID as Instrmt_ID",
+    "Instrmt.SecType as Instrmt_SecType",
+    "Instrmt.MMY as Instrmt_MMY",
+    "Instrmt.Exch as Instrmt_Exch",
+    "Instrmt.StrKPx as Instrmt_StrKPx",
+    "Instrmt.PutCall as Instrmt_PutCall",
+    "Instrmt.Fctr as Instrmt_Fctr",
+    "Instrmt.PCFctr as Instrmt_PCFctr",
+    "Undly.ID as Undly_ID",
+    "Undly.SecTyp as Undly_SecTyp",
+    "Undly.MMY as Undly_MMY",
+    "Undly.Exch as Undly_Exch",
+    "explode(Qty) as Qty"
+)
+
+# Select the columns in the desired order and rename them as needed
+output_df = flat_df.select(
+    "RptID",
+    "BizDt",
+    "Ccy",
+    "SetSesID",
+    "UndSetPx",
+    "AsgnMeth",
+    "PTY.ID",
+    "PTY.R",
+    "PTY.Sub.Id",
+    "PTY.Sub.type",
+    "Instrmt_ID",
+    "Instrmt_SecType",
+    "Instrmt_MMY",
+    "Instrmt_Exch",
+    "Instrmt_StrKPx",
+    "Instrmt_PutCall",
+    "Instrmt_Fctr",
+    "Instrmt_PCFctr",
+    "Undly_ID",
+    "Undly_SecTyp",
+    "Undly_MMY",
+    "Undly_Exch",
+    "Qty.Short",
+    "Qty.Typ"
+).toDF(*[field.name.replace(".", "_") for field in schema])
+
+output_df.show()
+
+
