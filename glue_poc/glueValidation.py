76be51cedf8fb35876300e22ca7ec70199109dc1
poccoder1 <poc.code.test@gmail.com>
2023-04-13 19:29:32 +0800

    test code for glue validation

--- glue_poc/glueValidation.py
+++ glue_poc/glueValidation.py
@@ -15,4 +15,9 @@ all_columns = get_all_columns(df.schema)
 print(all_columns)
 
 
-filtered_dict = {k: v for k, v in my_dict.items() if v.startswith('ArrayType[string]')}
+filtered_dict = {k: v for k, v in my_dict.items() if  v.startswith('ArrayType[string]')}
+
+
+for i, k in enumerate(my_dict):
+    key_list = [k2 for k2 in my_dict.keys() if k2 != k]
+    print(f"{k}: {key_list}")
