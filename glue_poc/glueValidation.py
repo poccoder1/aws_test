2eb328dc4d5e93d6a8ebcae71467d7c0a6d6d318
poccoder1 <poc.code.test@gmail.com>
2023-04-13 17:57:25 +0800

    test code for glue validation

--- glue_poc/glueValidation.py
+++ glue_poc/glueValidation.py
@@ -1,66 +1,58 @@
 from pyspark.sql.types import StructType, StructField, StringType, ArrayType, DoubleType
 
 # Define the schema for the XML file
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, ArrayType
+
 schema = StructType([
     StructField("Report", StructType([
         StructField("rptHdr", StructType([
-            StructField("exchNam", StringType(), True),
-            StructField("rptCod", StringType(), True)
-        ]), True),
+            StructField("exchNam", StringType()),
+            StructField("rptCod", StringType())
+        ])),
         StructField("reportNameGrp", StructType([
             StructField("cm", StructType([
                 StructField("rptSubHdr", StructType([
-                    StructField("membLglNam", StringType(), True),
-                    StructField("membId", StringType(), True)
-                ]), True),
-                StructField("acctTyGrp", ArrayType(StructType([
-                    StructField("Name", StringType(), True),
+                    StructField("membLglNam", StringType()),
+                    StructField("membId", StringType())
+                ])),
+                StructField("acctTyGrp", StructType([
                     StructField("LiquidationGrp", StructType([
-                        StructField("Name", StringType(), True),
+                        StructField("Name", StringType()),
+                        StructField("LiquAdj", ArrayType(StructType([
+                            StructField("component", StringType()),
+                            StructField("value", DoubleType())
+                        ])))
+                    ]))
+                ]))
+            ])),
+            StructField("RC", ArrayType(StructType([
+                StructField("rptSubHdr", StructType([
+                    StructField("membLglNam", StringType()),
+                    StructField("membId", StringType())
+                ])),
+                StructField("acctTyGrp", ArrayType(StructType([
+                    StructField("LiquidationGrp", StructType([
+                        StructField("Name", StringType()),
                         StructField("currMarComp", StructType([
-                            StructField("Name", StringType(), True),
-                            StructField("MarketRisk_Aggr_T", DoubleType(), True),
-                            StructField("MarketRisk_Aggr_T-1", DoubleType(), True),
+                            StructField("Name", StringType()),
+                            StructField("MarketRisk_Aggr_T", DoubleType()),
+                            StructField("MarketRisk_Aggr_T-1", DoubleType()),
                             StructField("LiquAdj", ArrayType(StructType([
-                                StructField("component", StringType(), True),
-                                StructField("value", DoubleType(), True)
-                            ]), True), True)
-                        ]), True),
+                                StructField("component", StringType()),
+                                StructField("value", DoubleType())
+                            ])))
+                        ])),
                         StructField("LiquAdj", ArrayType(StructType([
-                            StructField("component", StringType(), True),
-                            StructField("value", DoubleType(), True)
-                        ]), True), True)
-                    ]), True))
-            ]), True)
-        ]), True),
-        StructField("RC", ArrayType(StructType([
-            StructField("rptSubHdr", StructType([
-                StructField("membLglNam", StringType(), True),
-                StructField("membId", StringType(), True)
-            ]), True),
-            StructField("acctTyGrp", ArrayType(StructType([
-                StructField("Name", StringType(), True),
-                StructField("LiquidationGrp", StructType([
-                    StructField("Name", StringType(), True),
-                    StructField("currMarComp", StructType([
-                        StructField("Name", StringType(), True),
-                        StructField("MarketRisk_Aggr_T", DoubleType(), True),
-                        StructField("MarketRisk_Aggr_T-1", DoubleType(), True),
-                        StructField("LiquAdj", ArrayType(StructType([
-                            StructField("component", StringType(), True),
-                            StructField("value", DoubleType(), True)
-                        ]), True), True)
-                    ]), True),
-                    StructField("LiquAdj", ArrayType(StructType([
-                        StructField("component", StringType(), True),
-                        StructField("value", DoubleType(), True)
-                    ]), True), True)
-                ]), True))
-        ]), True))
-    ]), True)
-]), True)
-]), True)
+                            StructField("component", StringType()),
+                            StructField("value", DoubleType())
+                        ])))
+                    ]))
+                ])))
+            ]))
+        ]))
+    ]))
 ])
+
 
 # Read the XML file with the defined schema
 df = spark.read.format("com.databricks.spark.xml").option("rowTag", "Report").schema(schema).load("path/to/xml/file")
