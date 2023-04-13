1811496b3fa2ca4b5449dbc756ea9c65ec9f04cc
poccoder1 <poc.code.test@gmail.com>
2023-04-13 18:55:43 +0800

    test code for glue validation

--- glue_poc/glueValidation.py
+++ glue_poc/glueValidation.py
@@ -1,64 +1,15 @@
-from pyspark.sql.types import StructType, StructField, StringType, ArrayType, DoubleType
+from pyspark.sql.types import StructType
 
-# Define the schema for the XML file
-from pyspark.sql.types import StructType, StructField, StringType, DoubleType, ArrayType
+def get_all_columns(schema, prefix=''):
+    columns = []
+    for field in schema.fields:
+        name = prefix + field.name
+        if isinstance(field.dataType, StructType):
+            columns += get_all_columns(field.dataType, name + '.')
+        else:
+            columns.append(name)
+    return columns
 
-schema = StructType([
-    StructField("Report", StructType([
-        StructField("rptHdr", StructType([
-            StructField("exchNam", StringType()),
-            StructField("rptCod", StringType())
-        ])),
-        StructField("reportNameGrp", StructType([
-            StructField("cm", StructType([
-                StructField("rptSubHdr", StructType([
-                    StructField("membLglNam", StringType()),
-                    StructField("membId", StringType())
-                ])),
-                StructField("acctTyGrp", StructType([
-                    StructField("LiquidationGrp", StructType([
-                        StructField("Name", StringType()),
-                        StructField("LiquAdj", ArrayType(StructType([
-                            StructField("component", StringType()),
-                            StructField("value", DoubleType())
-                        ])))
-                    ]))
-                ]))
-            ])),
-            StructField("RC", ArrayType(StructType([
-                StructField("rptSubHdr", StructType([
-                    StructField("membLglNam", StringType()),
-                    StructField("membId", StringType())
-                ])),
-                StructField("acctTyGrp", ArrayType(StructType([
-                    StructField("LiquidationGrp", StructType([
-                        StructField("Name", StringType()),
-                        StructField("currMarComp", StructType([
-                            StructField("Name", StringType()),
-                            StructField("MarketRisk_Aggr_T", DoubleType()),
-                            StructField("MarketRisk_Aggr_T-1", DoubleType()),
-                            StructField("LiquAdj", ArrayType(StructType([
-                                StructField("component", StringType()),
-                                StructField("value", DoubleType())
-                            ])))
-                        ])),
-                        StructField("LiquAdj", ArrayType(StructType([
-                            StructField("component", StringType()),
-                            StructField("value", DoubleType())
-                        ])))
-                    ]))
-                ])))
-            ]))
-        ]))
-    ]))
-])
-
-
-# Read the XML file with the defined schema
-df = spark.read.format("com.databricks.spark.xml").option("rowTag", "Report").schema(schema).load("path/to/xml/file")
-
-# Get the array type fields
-array_fields = [field.name for field in df.schema.fields if isinstance(field.dataType, ArrayType)]
-
-# Print the array type fields
-print(array_fields)
+# Assuming df is your PySpark DataFrame
+all_columns = get_all_columns(df.schema)
+print(all_columns)
