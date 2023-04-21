660d36e332425f69445ffcc12a7a0b6dbefa4dbc
poccoder1 <poc.code.test@gmail.com>
2023-04-21 17:07:19 +0800

    test code for glue validation

--- glue_poc/glueValidation.py
+++ glue_poc/glueValidation.py
@@ -1,59 +1,26 @@
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DateType, FloatType
+import json
+import csv
+import pandas as pd
 
-schema = StructType([
-    StructField("Batch", StructType([
-        StructField("PostRpt", StructType([
-            StructField("@RptID", StringType()),
-            StructField("@BizDt", DateType()),
-            StructField("@SetPx", FloatType()),
-            StructField("Pty", StructType([
-                StructField("@ID", StringType()),
-                StructField("@R", IntegerType()),
-                StructField("Sub", StructType([
-                    StructField("@ID", IntegerType()),
-                    StructField("@Typ", StringType())
-                ]))
-            ])),
-            StructField("Instrmt", StructType([
-                StructField("@ID", StringType()),
-                StructField("@SecTyp", StringType()),
-                StructField("@MMY", StringType()),
-                StructField("@Exch", StringType()),
-                StructField("@UOM", StringType())
-            ])),
-            StructField("Qty", StructType([
-                StructField("@Long", IntegerType()),
-                StructField("@Short", IntegerType()),
-                StructField("@Typ", StringType())
-            ])),
-            StructField("Amt", StructType([
-                StructField("@Typ", StringType()),
-                StructField("@Amt", FloatType()),
-                StructField("@Ccy", StringType())
-            ]))
-        ])),
-        StructField("TrdCaptRpt", StructType([
-            StructField("@RptID", StringType()),
-            StructField("@BizDt", DateType()),
-            StructField("Instrmt", StructType([
-                StructField("@ID", StringType()),
-                StructField("@Fctr", IntegerType())
-            ])),
-            StructField("Amt", StructType([
-                StructField("@Typ", StringType()),
-                StructField("@Amt", FloatType()),
-                StructField("@Ccy", StringType())
-            ])),
-            StructField("Pty", StructType([
-                StructField("@ID", StringType()),
-                StructField("@R", IntegerType()),
-                StructField("Sub", StructType([
-                    StructField("@ID", IntegerType()),
-                    StructField("@Typ", StringType())
-                ]))
-            ])),
-            StructField("RegTrdID", StructType([
-                StructField("@ID", StringType())
-            ]))
-        ]))
-    ]))
+# Load JSON config
+with open('config.json') as f:
+    config = json.load(f)
+
+# Get schema definition length for Fix_Delimited source
+schema_definition_length = config['producer']['Fix_Delimited']['schemaDefinitionLength']
+
+# Read CSV file into a DataFrame
+df = pd.read_csv('data.csv')
+
+# Iterate through columns in the DataFrame
+for col_name in df.columns:
+    # Check if column is specified in schema definition
+    if col_name in schema_definition_length:
+        col_len = schema_definition_length[col_name]
+        # Trim data if length specified in schema definition
+        df[col_name] = df[col_name].astype(str).str[:col_len]
+        # Add whitespace padding if data is shorter than specified length
+        df[col_name] = df[col_name].str.ljust(col_len)
+
+# Write the DataFrame to a CSV file
+df.to_csv('processed_data.csv', index=False)
