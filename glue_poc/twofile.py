dca2aab9c9672248e106013199d0ba31121e6dbb
poccoder1 <poc.code.test@gmail.com>
2023-05-18 23:07:04 +0800

    test code for glue validation

--- glue_poc/twofile.py
+++ glue_poc/twofile.py
@@ -39,3 +39,155 @@ output_dynamic_frame = DynamicFrame.fromDF(output_df, glueContext, "output_dynam
 
 # Write the dynamic frame to S3 as a CSV file
 glueContext.write_dynamic_frame.from_options(output_dynamic_frame, connection_type="s3", connection_options={"path": "s3://your_output_bucket/path/to/output/file"}, format="csv")
+
+
+====================================================================
+
+import sys
+import json
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from pyspark.sql import SparkSession
+
+# Initialize the Spark and Glue contexts
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+# Obtain the AWS Glue job arguments
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+# Read the configuration file
+with open('config.json') as f:
+    config = json.load(f)
+
+# Process each file in the configuration
+for file_config in config['files']:
+    file_path = file_config['path']
+    table_name = file_config['table_name']
+    join_columns = file_config['join_columns']
+
+    # Create a dynamic frame for the current file
+    dynamic_frame = glueContext.create_dynamic_frame.from_catalog(database="your_database_name", table_name=table_name, transformation_ctx=table_name)
+    data_frame = dynamic_frame.toDF()
+
+    # Register the current file as a temporary table
+    data_frame.createOrReplaceTempView(table_name)
+
+# Perform the join operations
+join_query = ''
+for join_config in config['join_conditions']:
+    join_table = join_config['table']
+    join_columns1 = join_config['columns1']
+    join_columns2 = join_config['columns2']
+
+    # Build the join condition
+    join_condition = ' AND '.join(f"{table}.{col1} = {join_table}.{col2}" for col1, col2 in zip(join_columns1, join_columns2))
+    join_query += f"JOIN {join_table} ON {join_condition} "
+
+# Perform the join query
+joined_df = spark.sql(f"SELECT * FROM {config['files'][0]['table_name']} {join_query}")
+
+# Select the required columns
+output_df = joined_df.select(*[col['table'] + '.' + col['column'] for col in config['output_columns']])
+
+# Convert the DataFrame back to a dynamic frame
+output_dynamic_frame = DynamicFrame.fromDF(output_df, glueContext, "output_dynamic_frame")
+
+# Write the dynamic frame to S3 as a CSV file
+glueContext.write_dynamic_frame.from_options(output_dynamic_frame, connection_type="s3", connection_options={"path": "s3://your_output_bucket/path/to/output/file"}, format="csv")
+
+
+## json
+
+{
+    "files": [
+        {
+            "path": "s3://your_bucket/file1",
+            "table_name": "file1_table",
+            "join_columns": ["col1", "col2"]
+        },
+        {
+            "path": "s3://your_bucket/file2",
+            "table_name": "file2_table",
+            "join_columns": ["col4", "col5"]
+        }
+    ],
+    "join_conditions": [
+        {
+            "table": "file1_table",
+            "columns1": ["col1", "col2"],
+            "columns2": ["col4", "col5"]
+        }
+    ],
+    "output_columns": [
+        {
+            "table": "file1_table",
+            "column": "col1"
+        },
+        {
+            "table": "file1_table",
+            "column": "col2"
+        },
+        {
+            "table": "
+
+===
+==============================================================  without table===
+
+import sys
+import json
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from pyspark.sql import SparkSession
+
+# Initialize the Spark and Glue contexts
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+# Obtain the AWS Glue job arguments
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+# Read the configuration file
+with open('config.json') as f:
+    config = json.load(f)
+
+# Read each file from the configuration
+data_frames = {}
+for file_config in config['files']:
+    file_path = file_config['path']
+df = spark.read.option("header", "true").csv(file_path)
+table_name = file_config['table_name']
+data_frames[table_name] = df
+
+# Perform the join operations
+joined_df = None
+for join_config in config['join_conditions']:
+    table1 = join_config['table1']
+table2 = join_config['table2']
+join_columns1 = join_config['columns1']
+join_columns2 = join_config['columns2']
+
+# Perform the join operation
+join_condition = [df1[col1] == df2[col2] for col1, col2 in zip(join_columns1, join_columns2)]
+join_type = join_config.get('type', 'inner')  # Default to inner join if not specified
+join_df = data_frames[table1].join(data_frames[table2], join_condition, join_type)
+
+# Update the joined DataFrame
+if joined_df is None:
+    joined_df = join_df
+else:
+    joined_df = joined_df.join(join_df, join_condition, join_type)
+
+# Select the required columns
+output_columns = [f"{table}.{col}" for table, col in config['output_columns']]
+output_df = joined_df.select(*output_columns)
+
+# Write the DataFrame to S3 as a CSV file
+output_path = config['output_path']
+output_df.write.option("header", "true").csv(output_path)
