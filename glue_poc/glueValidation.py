6471d5e8bc7931b093a63f42eeca2546908c4ac3
poccoder1 <poc.code.test@gmail.com>
2023-05-28 13:42:44 +0800

    test code for glue validation

--- glue_poc/glueValidation.py
+++ glue_poc/glueValidation.py
@@ -1,56 +1,80 @@
-AWS Glue is a fully managed extract, transform, and load (ETL) service provided by Amazon Web Services (AWS). It offers a serverless environment for preparing and transforming data for analytics, machine learning, and other data processing workloads. Glue simplifies and automates the process of discovering, cataloging, cleaning, and transforming data, making it easier to analyze and derive insights from your data.
 
-Use Cases of AWS Glue:
+========Teamwork======
+My performance goal revolves around mentoring and teamwork, where I strive to provide assistance to others and foster a collaborative environment. I consistently extend my support to colleagues by dedicating late nights to resolving their issues. Additionally, I actively engage with my team members, discussing my approach and seeking their input to encourage a collective decision-making process.
 
-1. Data Preparation and ETL: AWS Glue simplifies the process of transforming and cleaning data to make it suitable for analysis. It offers a visual ETL interface and a rich set of transformation capabilities, such as filtering, mapping, aggregating, and joining data. Glue generates Scala or Python code based on your transformations, which can be optimized and executed at scale.
+When it comes to working with new technologies, I take the initiative to explain their benefits and explore how they can be effectively utilized. I ensure that I provide detailed explanations, highlighting the advantages and showcasing practical examples of how the technology can be implemented. By doing so, I aim to empower my team with the knowledge and understanding necessary to embrace and leverage these new advancements.
 
-Data Integration: Glue can be used to integrate data from various sources, such as databases, data lakes, and streaming platforms. It enables you to transform and combine data from different formats and structures into a unified format for analysis and processing.
+Overall, my performance goal is to promote mentorship and teamwork by consistently assisting others, fostering open communication, and actively sharing knowledge about new technologies.
 
-Data Warehousing: Glue can extract data from various sources and load it into a data warehouse such as Amazon Redshift or Amazon Athena. It automates the schema discovery process and performs transformations to ensure data compatibility with the target data warehouse.
+=====Additional details
 
-Data Lakes and Analytics: Glue can help in building and managing data lakes on Amazon S3. It can automatically catalog the data and make it available for querying using services like Athena or Amazon Redshift Spectrum. Glue also supports building ETL pipelines for processing and transforming data within the data lake.
+Act as a resource: Alongside resolving issues and discussing approaches, I make myself readily available as a resource for my colleagues. I encourage them to approach me with any questions or concerns, providing guidance and support whenever needed.
 
-Machine Learning: Glue can prepare and transform data for machine learning workflows. It can help in cleaning and preprocessing data, performing feature engineering, and preparing the dataset for training machine learning models.
+Share best practices: In addition to resolving immediate issues, I take the opportunity to share best practices with my team. I draw from my own experiences and research to identify efficient and effective approaches, which I then communicate to the team. This helps improve overall productivity and fosters a culture of continuous learning.
 
-Advantages of AWS Glue:
+Encourage knowledge sharing: I actively encourage knowledge sharing within the team. During team meetings or informal discussions, I initiate conversations that promote the exchange of ideas, lessons learned, and innovative solutions. By creating an environment where everyone feels comfortable sharing their knowledge and experiences, we collectively benefit from a wealth of expertise.
 
-Fully Managed Service: AWS Glue is a fully managed service, which means AWS takes care of the infrastructure and server management. This eliminates the need for manual provisioning and scaling, allowing you to focus on data processing and analytics.
+Provide constructive feedback: As part of my mentoring approach, I provide constructive feedback to my colleagues. Whether it's on their work, problem-solving methods, or professional development, I offer guidance to help them improve their skills and achieve their goals. I focus on providing specific, actionable feedback that encourages growth and fosters a positive and supportive atmosphere.
 
-Serverless Environment: Glue operates in a serverless manner, automatically scaling resources based on demand. This ensures high availability and allows you to pay only for the resources consumed during data processing.
-
-Data Catalog and Metadata Management: Glue provides a centralized data catalog, the AWS Glue Data Catalog, where metadata about your data sources, tables, and schemas is stored. This makes it easy to discover, understand, and share data across different AWS services.
-
-Schema Discovery and Crawlers: Glue's schema discovery capability can automatically infer the schema of your data sources, including databases, data lakes, and streaming platforms. Glue crawlers can scan and catalog the data, making it available for querying and processing.
-
-ETL Automation: Glue simplifies the ETL process by providing a visual interface for creating and managing ETL jobs. It offers pre-built transformations and supports popular programming languages like Python and Scala, making it easier to process and transform data.
-
-Disadvantages of AWS Glue:
-
-Learning Curve: Glue has a learning curve, especially if you are new to ETL concepts and AWS services. It requires understanding the Glue components, workflows, and configuration options to effectively use the service.
-
-Limited Customization: While Glue provides pre-built transformations and supports popular programming languages, it may have limitations when it comes to advanced or highly customized data processing scenarios. In such cases, you may need to rely on custom code or other AWS services.
-
-Cost: As with any managed service, there are costs associated with using AWS Glue. You pay for the resources consumed during data processing, as well as for the storage used by the AWS Glue Data Catalog. It's important to monitor and optimize your resource usage to control costs effectively.
-
-==========
-
-When working with AWS Glue, there are two main approaches to access data stored in Amazon S3:
-
-Directly accessing files from S3: In this approach, you can read the files stored in S3 directly using AWS Glue's built-in connectors. You can create a Glue job that reads the data from S3, performs transformations, and processes it. This allows you to work with the data in a DataFrame-like manner within the Glue job. However, the data is not cataloged in the AWS Glue Data Catalog.
-
-Using a Crawler and Data Catalog: In this approach, you create an AWS Glue crawler, which is a service that automatically discovers and catalogs the metadata of your data in various sources, including S3. The crawler scans the S3 data, identifies the schema, and creates a table in the AWS Glue Data Catalog. The table represents the data stored in S3, and the catalog contains metadata such as table name, column names, data types, and partition information.
-
-The main differences between using direct files from S3 and using the crawler and table approach are as follows:
-
-Metadata and Cataloging: When you use a crawler and table, the metadata about the data, including the schema, is stored in the AWS Glue Data Catalog. This metadata can be used by other AWS services, such as AWS Athena or Amazon Redshift Spectrum, to query the data in a more SQL-like manner. It also provides a centralized catalog that can be shared across multiple jobs or services.
-
-Schema Consistency: When using a crawler and table, the schema of the data is determined automatically by the crawler based on the actual data in S3. This helps in maintaining schema consistency, especially when dealing with evolving data sources where the schema can change over time. In contrast, when reading files directly, the schema needs to be explicitly defined or inferred within the Glue job code.
-
-Data Discoverability: With the crawler and table approach, the data becomes more discoverable and searchable within the AWS Glue ecosystem. You can use the Data Catalog to search for tables, inspect their schemas, and view sample data. This can be useful for data exploration and understanding the available datasets.
-
-Separation of Concerns: Using the crawler and table approach allows you to decouple the data cataloging process from the actual data processing logic. This separation of concerns enables more flexibility and reusability, as the data catalog can be leveraged by different jobs, services, or tools independently.
-
-In summary, while both approaches allow you to work with data from S3 in AWS Glue, using a crawler and table provides the benefits of centralized metadata management, schema consistency, data discoverability, and separation of concerns. However, it may introduce some additional setup and maintenance overhead compared to directly reading files from S3. The choice between the two approaches depends on your specific use case and requirements.
+Act as a mentor for new team members: When new team members join, I take on the role of a mentor, guiding them through the onboarding process and helping them acclimate to the team and company culture. I share my knowledge and experiences, offer support, and provide resources to facilitate their integration into the team smoothly.
 
 
-==========================
+
+
+
+
+================================production support =======
+
+
+Independent Issue Resolution:
+During the learning phase, I actively seek opportunities to enhance my technical expertise and problem-solving skills. When confronted with support issues, I adopt a proactive approach to quickly identify and resolve them. I employ thorough investigation techniques, leveraging available resources such as logs, documentation, and knowledge bases. By taking ownership of issues, I strive to minimize the impact on production and ensure a smooth user experience.
+
+Collaboration with Team Members:
+    Recognizing the importance of teamwork, I maintain open lines of communication with my colleagues. I actively engage in knowledge-sharing sessions, seeking insights and advice from more experienced team members. By collaborating effectively, I tap into the collective expertise of the team, enabling faster and more accurate issue resolution. Additionally, I offer my assistance to fellow team members, fostering a supportive and cooperative work environment.
+
+Proactive Development Practices:
+To ensure production stability, I adopt a proactive mindset during the development phase. Before writing code, I thoroughly analyze requirements and engage in detailed discussions with the team to identify potential corner cases. By considering a wide range of scenarios, I strive to develop robust and resilient solutions that minimize the likelihood of production issues. Through careful planning and diligent coding, I aim to deliver software that meets high-quality standards and is less prone to errors.
+
+Adherence to Coding Standards:
+Recognizing the importance of maintainable and reliable code, I strictly adhere to established coding standards and best practices. I diligently follow coding conventions, ensuring consistency and readability in the codebase. By maintaining clean and organized code, I facilitate easier troubleshooting and future enhancements. Moreover, I prioritize the use of defensive programming techniques, implementing error handling mechanisms and input validation to prevent unexpected failures.
+
+Code Review and Merge Request Process:
+As an integral part of ensuring production stability, I actively participate in code reviews and merge request processes. I approach these activities with thoroughness and attention to detail, carefully examining the code for potential issues, such as performance bottlenecks, security vulnerabilities, or logic errors. Additionally, I provide constructive feedback to my colleagues, offering suggestions for improvements and ensuring compliance with coding standards. Through this meticulous review process, we collectively strive to deliver production-ready code that minimizes the likelihood of post-deployment issues.
+
+
+========== AWS migration======
+I wanted to provide an update on my performance goals as part of the AWS migration project. Although I did not have prior experience with AWS migration, I took the initiative to work on AWS Glue along with Python and Spark. These technologies were completely new to me, and I dedicated numerous late nights and weekends to understand AWS Glue and create a demo proof of concept (POC).
+
+My focus primarily revolved around AWS Glue and involved creating Terraform code for the Glue pipeline. Additionally, I worked with other AWS services such as S3, Lambda, and established database connectivity with Amazon Aurora MySQL and DynamoDB. One of the challenging aspects of my work was dealing with XML extraction in the Glue ETL process, but I was able to overcome this hurdle successfully. I am grateful for the support provided by Kexin.
+
+To optimize our ETL processes, I also took the initiative to create a dynamic framework that can support multiple file ETL processes. This framework enhances flexibility and reusability, allowing for greater efficiency and scalability in our workflows.
+
+Furthermore, I actively collaborated with other teams throughout the project, assisting them in fitting their requirements into the AWS Glue ecosystem and addressing any issues they encountered. This teamwork and support were essential in ensuring the smooth progress of the AWS migration.
+
+Overall, I am pleased to report that I have made significant progress in mastering AWS Glue and Python with Spark. I have created a demo POC, developed Terraform code for the Glue pipeline, integrated various AWS services, addressed the challenges posed by XML extraction, and established a dynamic framework for multiple file ETL processes. I am confident that these efforts will contribute positively to the success of our AWS migration project.
+
+Thank you for your support and guidance throughout this process. If there are any additional details or areas you would like me to focus on, please let me know. I am eager to continue learning and contributing to the team's success.
+
+
+===========Application mode=====
+Over the course of this period, I have made significant contributions that have positively impacted our application's stability, performance, and deployment efficiency.
+
+One of the key improvements I implemented was the integration of Liquibase, which has greatly facilitated automatic deployment processes. By utilizing Liquibase, we have been able to streamline our deployment workflows, reducing manual effort and increasing efficiency. This implementation has contributed to faster and more reliable deployments, resulting in improved overall productivity and reduced risk of errors.
+
+Additionally, I successfully developed a highly complex Zip file extractor component as part of our application modernization efforts. This extractor not only allows us to extract files from Zip archives but also validates them during subsequent batch runs. The intricate nature of this component required extensive analysis and understanding of various scenarios. I am grateful for the support provided by my colleague [Teammate's Name], who thoroughly explained and documented the different scenarios. This collaboration ensured that the Zip file extractor is robust and capable of handling diverse file extraction requirements.
+
+As an active participant in application architecture discussions, I consistently strive to contribute my expertise to enhance application stability and performance. I proactively offer input and suggestions to promote a stable and efficient application design. By being involved in these discussions, I ensure that our applications are built with scalability, reliability, and maintainability in mind.
+
+Moreover, I continually prioritize application stability and performance in my work. I regularly provide insights and suggestions to optimize code, improve database queries, and enhance overall system efficiency. By diligently focusing on these aspects, I aim to deliver a high-performing application that meets both user expectations and business requirements.
+
+
+
+    ================vul=====
+
+To achieve this goal, I regularly perform vulnerability scans and assessments on the jar files used in our application. These scans help identify any known security weaknesses or vulnerabilities that may exist in the versions of the jar files we currently have in use.
+
+Once a vulnerability is detected, my next step is to find the correct and most up-to-date version of the jar file that resolves the identified vulnerability. This involves researching and analyzing various sources, such as vendor websites, security advisories, and vulnerability databases, to determine the appropriate version to upgrade to.
+
+In addition to addressing vulnerabilities, I also consider the overall stability and compatibility of the application. Since we utilize other applications as parent jars in our system, it is essential to keep them up to date as well. By using the latest version of the parent jar, we ensure that any new features or improvements introduced by the parent application are readily available to our application.
+
+This proactive approach helps mitigate security risks by reducing the likelihood of known vulnerabilities being exploited. It also ensures that our application remains compatible with the latest features and updates provided by the parent application, thus enhancing its overall functionality and performance.
