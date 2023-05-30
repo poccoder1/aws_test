1a7ef928ea806bbd46fadb60425e865ba4cb32f0
poccoder1 <poc.code.test@gmail.com>
2023-05-30 14:41:56 +0800

    test code for glue validation

--- glue_poc/glueValidation.py
+++ glue_poc/glueValidation.py
@@ -2,4 +2,10 @@ dynamic_frame = glueContext.create_dynamic_frame.from_catalog(frame=input_df, da
 
 glueContext.write_dynamic_frame.from_catalog(frame=dynamic_frame, database='your_database_name', table_name='your_table_name')
 
-job.commit()
+glueContext.write_dynamic_frame.from_catalog(
+    frame=dynamic_frame,
+    database=glue_database,
+    table_name=glue_table,
+    transformation_ctx="write_dynamic_frame",
+    additional_options={"partitionKeys": partition_keys}
+)
