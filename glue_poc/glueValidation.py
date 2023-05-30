261971c7b412b21e272708c488d2728b9ff13a46
poccoder1 <poc.code.test@gmail.com>
2023-05-30 16:20:07 +0800

    test code for glue validation

--- glue_poc/glueValidation.py
+++ glue_poc/glueValidation.py
@@ -1,11 +1,39 @@
-dynamic_frame = glueContext.create_dynamic_frame.from_catalog(frame=input_df, database='your_database_name', table_name='your_table_name')
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from pyspark.sql.functions import *
 
-glueContext.write_dynamic_frame.from_catalog(frame=dynamic_frame, database='your_database_name', table_name='your_table_name')
+# Initialize GlueContext and SparkContext
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
 
-glueContext.write_dynamic_frame.from_catalog(
-    frame=dynamic_frame,
-    database=glue_database,
-    table_name=glue_table,
-    transformation_ctx="write_dynamic_frame",
-    additional_options={"partitionKeys": partition_keys}
-)
+# Get job parameters
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'input_bucket', 'input_key', 'output_bucket', 'output_key', 'glue_database', 'glue_table'])
+
+# Read input file from S3
+input_bucket = args['input_bucket']
+input_key = args['input_key']
+input_path = "s3://{}/{}".format(input_bucket, input_key)
+input_df = spark.read.csv(input_path, header=True, inferSchema=True)
+
+# Perform ETL operations
+# Example: Convert a column to uppercase
+output_df = input_df.withColumn("column_name", upper(col("column_name")))
+
+# Store output to S3
+output_bucket = args['output_bucket']
+output_key = args['output_key']
+output_path = "s3://{}/{}".format(output_bucket, output_key)
+output_df.write.csv(output_path, header=True, mode="overwrite")
+
+# Create or update Glue table
+glue_database = args['glue_database']
+glue_table = args['glue_table']
+glueContext.create_dynamic_frame.from_catalog(database=glue_database, table_name=glue_table)
+glueContext.write_dynamic_frame.from_catalog(frame=output_df, database=glue_database, table_name=glue_table)
+
+# Commit the job
+job.commit()
