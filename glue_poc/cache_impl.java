ae0250a0c9bc1dbdb519f3243915095541101008
poccoder1 <poc.code.test@gmail.com>
2023-08-30 08:56:49 +0800

    test code for glue validation

--- glue_poc/cache_impl.java
+++ glue_poc/cache_impl.java
@@ -1,134 +1,258 @@
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import com.github.benmanes.caffeine.cache.Caffeine;
-import java.util.concurrent.TimeUnit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
 
-@Configuration
-@EnableCaching
-public class CacheConfig {
+import java.util.Map;
 
-    @Bean
-    public CacheManager cacheManager() {
-        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
-        cacheManager.setCaffeine(Caffeine.newBuilder()
-                .expireAfterWrite(Long.MAX_VALUE, TimeUnit.NANOSECONDS) // Set to a very large value
-                .maximumSize(1) // Store a single entry for the HashMap
-                .maximumWeight(300 * 1024 * 1024) // Max size in bytes (300MB)
-                .build());
-        return cacheManager;
+@Service
+public class AccountService {
+
+    @Autowired
+    private ExternalApiService externalApiService;
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @Cacheable("accountCache")
+    public Map<String, Account> getAllAccounts() {
+        return externalApiService.fetchAllAccounts();
+    }
+
+    public Account getAccountById(String accountId) {
+        Map<String, Account> accountMap = getAllAccounts();
+        Account account = accountMap.get(accountId);
+        if (account == null) {
+            // Account not found in cache, fetch it from the external API
+            account = externalApiService.fetchAccountById(accountId);
+
+            // Update the cache with the new account data
+            cacheManager.getCache("accountCache").put(accountId, account);
+        }
+        return account;
+    }
+
+    // Optional: You can use this method to manually evict a specific account from the cache
+    @CacheEvict(value = "accountCache", key = "#accountId")
+    public void evictAccountFromCache(String accountId) {
     }
 }
 
 
-========
 
-        import org.slf4j.Logger;
-        import org.slf4j.LoggerFactory;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.cache.Cache;
-        import org.springframework.cache.CacheManager;
+==============
+
         import org.springframework.stereotype.Service;
-        import org.springframework.web.client.HttpClientErrorException;
         import org.springframework.web.client.RestTemplate;
-
-        import java.util.HashMap;
-        import java.util.List;
         import java.util.Map;
 
 @Service
-public class CustomerService {
+public class ExternalApiService {
 
-    private static final Logger log = LoggerFactory.getLogger(CustomerService.class);
+    private final String externalApiUrl = "YOUR_EXTERNAL_API_URL";
 
-    private final CacheManager cacheManager;
-    private final RestTemplate restTemplate;
-
-    @Autowired
-    public CustomerService(CacheManager cacheManager, RestTemplate restTemplate) {
-        this.cacheManager = cacheManager;
-        this.restTemplate = restTemplate;
+    public Map<String, Account> fetchAllAccounts() {
+        RestTemplate restTemplate = new RestTemplate();
+        // Make a REST API call to fetch all accounts
+        // Map the response to Map<String, Account>
+        // Example: Map<String, Account> accounts = restTemplate.getForObject(externalApiUrl, Map.class);
+        // Note: Replace the above line with the actual API call and response mapping logic.
+        return accounts;
     }
-
-    public Map<Long, Customer> getCustomersByIds(List<Long> customerIds) {
-        Map<Long, Customer> customersMap = new HashMap<>();
-
-        for (Long customerId : customerIds) {
-            Customer customer = getCustomerById(customerId);
-            if (customer != null) {
-                customersMap.put(customerId, customer);
-            }
-        }
-
-        return customersMap;
-    }
-
-    public Customer getCustomerById(Long customerId) {
-        Cache cache = cacheManager.getCache("customers");
-        if (cache != null) {
-            Cache.ValueWrapper valueWrapper = cache.get("customerMap");
-            if (valueWrapper != null) {
-                Map<Long, Customer> cachedCustomerMap = (Map<Long, Customer>) valueWrapper.get();
-                if (cachedCustomerMap != null) {
-                    if (cachedCustomerMap.containsKey(customerId)) {
-                        Customer customer = cachedCustomerMap.get(customerId);
-                        log.info("Customer with ID {} found in cache.", customerId);
-                        return customer;
-                    }
-                }
-            }
-        }
-
-        Customer customer = fetchCustomerDataFromAPI(customerId);
-        if (customer != null) {
-            updateCustomerInCache(customerId, customer);
-        }
-
-        return customer;
-    }
-
-    private Customer fetchCustomerDataFromAPI(Long customerId) {
-        try {
-            // Replace the URL with your actual API endpoint for fetching customer data
-            String apiUrl = "https://your-api.com/customers/" + customerId;
-            Customer customer = restTemplate.getForObject(apiUrl, Customer.class);
-            log.info("Fetched customer data for ID {} from external API.", customerId);
-            return customer;
-        } catch (HttpClientErrorException.NotFound notFoundException) {
-            log.error("Customer with ID {} not found in the external API.", customerId);
-            return null;
-        } catch (Exception e) {
-            log.error("Failed to fetch customer data from the external API for ID {}.", customerId, e);
-            return null;
-        }
-    }
-
-    private void updateCustomerInCache(Long customerId, Customer customer) {
-        Cache cache = cacheManager.getCache("customers");
-        if (cache != null) {
-            Cache.ValueWrapper valueWrapper = cache.get("customerMap");
-            if (valueWrapper != null) {
-                Map<Long, Customer> cachedCustomerMap = (Map<Long, Customer>) valueWrapper.get();
-                if (cachedCustomerMap != null) {
-                    cachedCustomerMap.put(customerId, customer);
-                    cache.put("customerMap", cachedCustomerMap);
-                } else {
-                    // If the cache is empty, create a new map with the customer and store it in the cache
-                    Map<Long, Customer> newCustomerMap = new HashMap<>();
-                    newCustomerMap.put(customerId, customer);
-                    cache.put("customerMap", newCustomerMap);
-                }
-            } else {
-                // If the cache value is not found, create a new map with the customer and store it in the cache
-                Map<Long, Customer> newCustomerMap = new HashMap<>();
-                newCustomerMap.put(customerId, customer);
-                cache.put("customerMap", newCustomerMap);
-            }
-        }
-    }
-
 }
 
 
+==========
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.cache.annotation.CacheEvict;
+        import org.springframework.cache.annotation.Cacheable;
+        import org.springframework.stereotype.Service;
+
+        import java.util.Map;
+
+@Service
+public class AccountService {
+
+    @Autowired
+    private ExternalApiService externalApiService;
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @Cacheable("accountCache")
+    public Map<String, Account> getAllAccounts() {
+        return externalApiService.fetchAllAccounts();
+    }
+
+    public Account getAccountById(String accountId) {
+        Map<String, Account> accountMap = getAllAccounts();
+        return accountMap.get(accountId);
+    }
+
+    // This method will force refresh of the cache by calling the external API
+    public void refreshAccountCache() {
+        Map<String, Account> refreshedAccounts = externalApiService.fetchAllAccounts();
+        cacheManager.getCache("accountCache").clear(); // Clear the existing cache
+        cacheManager.getCache("accountCache").putAll(refreshedAccounts); // Update cache with new data
+    }
+
+    // Optional: You can use this method to manually evict a specific account from the cache
+    @CacheEvict(value = "accountCache", key = "#accountId")
+    public void evictAccountFromCache(String accountId) {
+    }
+}
+
+
+=========
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.scheduling.annotation.Scheduled;
+        import org.springframework.stereotype.Component;
+
+@Component
+public class CacheRefreshScheduler {
+
+    @Autowired
+    private AccountService accountService;
+
+    @Scheduled(cron = "0 0 0 * * ?") // Run every midnight
+    public void refreshCache() {
+        // Fetch new account data from the external API and update the cache
+        accountService.refreshAccountCache();
+    }
+}
+
+
+=======
+
+        import org.junit.jupiter.api.Test;
+        import org.mockito.Mockito;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.boot.test.context.SpringBootTest;
+        import org.springframework.boot.test.mock.mockito.MockBean;
+        import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:test.properties") // Use a test properties file if needed
+public class CacheRefreshSchedulerTest {
+
+    @Autowired
+    private CacheRefreshScheduler cacheRefreshScheduler;
+
+    @MockBean
+    private AccountService accountService;
+
+    @Test
+    public void testRefreshCache() {
+        // Mock the behavior of accountService.refreshAccountCache()
+        Mockito.doNothing().when(accountService).refreshAccountCache();
+
+        // Trigger the scheduler's refreshCache method
+        cacheRefreshScheduler.refreshCache();
+
+        // Verify that accountService.refreshAccountCache() was called once
+        Mockito.verify(accountService, Mockito.times(1)).refreshAccountCache();
+    }
+}
+
+
+=========
+
+        import org.junit.jupiter.api.Test;
+        import org.mockito.Mock;
+        import org.mockito.Mockito;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.boot.test.context.SpringBootTest;
+        import org.springframework.cache.Cache;
+        import org.springframework.cache.CacheManager;
+        import org.springframework.boot.test.mock.mockito.MockBean;
+
+        import java.util.HashMap;
+        import java.util.Map;
+
+@SpringBootTest
+public class AccountServiceTest {
+
+    @Autowired
+    private AccountService accountService;
+
+    @MockBean
+    private ExternalApiService externalApiService;
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @Test
+    public void testGetAllAccountsCacheHit() {
+        // Mock cache behavior
+        Cache cache = cacheManager.getCache("accountCache");
+        Map<String, Account> cachedAccounts = new HashMap<>();
+        cachedAccounts.put("1", new Account("1", "Account 1"));
+        cache.put("accountCache", cachedAccounts);
+
+        // Call the service method
+        Map<String, Account> accounts = accountService.getAllAccounts();
+
+        // Verify that the cached data is returned
+        Mockito.verify(externalApiService, Mockito.never()).fetchAllAccounts();
+        // Add additional assertions as needed
+    }
+
+    @Test
+    public void testGetAllAccountsCacheMiss() {
+        // Mock the behavior of externalApiService
+        Map<String, Account> freshAccounts = new HashMap<>();
+        freshAccounts.put("1", new Account("1", "Account 1"));
+        Mockito.when(externalApiService.fetchAllAccounts()).thenReturn(freshAccounts);
+
+        // Call the service method
+        Map<String, Account> accounts = accountService.getAllAccounts();
+
+        // Verify that the externalApiService.fetchAllAccounts() was called once
+        Mockito.verify(externalApiService, Mockito.times(1)).fetchAllAccounts();
+        // Add additional assertions as needed
+    }
+}
+
+
+=====
+        import org.junit.jupiter.api.Test;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.boot.test.context.SpringBootTest;
+        import org.springframework.boot.web.client.RestTemplateBuilder;
+        import org.springframework.boot.test.mock.mockito.MockBean;
+        import org.springframework.web.client.RestTemplate;
+
+        import java.util.HashMap;
+        import java.util.Map;
+
+        import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class ExternalApiServiceTest {
+
+    @Autowired
+    private ExternalApiService externalApiService;
+
+    @MockBean
+    private RestTemplateBuilder restTemplateBuilder;
+
+    @Test
+    public void testFetchAllAccounts() {
+        // Mock the behavior of the RestTemplate (you can customize this based on your actual implementation)
+        RestTemplate restTemplate = mock(RestTemplate.class);
+        when(restTemplateBuilder.build()).thenReturn(restTemplate);
+
+        // Mock the response from the external API
+        Map<String, Account> expectedAccounts = new HashMap<>();
+        expectedAccounts.put("1", new Account("1", "Account 1"));
+        when(restTemplate.getForObject(anyString(), eq(Map.class))).thenReturn(expectedAccounts);
+
+        // Call the service method
+        Map<String, Account> accounts = externalApiService.fetchAllAccounts();
+
+        // Verify that the expected data is returned
+        // Add additional assertions as needed
+    }
+
+    // Similar tests for fetchAccountById can be added
+}
