f0f47232428ef5eb777851104e8be6a74977b57b
poccoder1 <poc.code.test@gmail.com>
2023-10-30 23:13:03 +0800

    test code for glue validation

---
+++ compatableFutur/parquet.java
@@ -0,0 +1,15 @@
+Business Results:
+
+        Exchange Mandatory Changes:
+
+        Successfully delivered mandatory changes in line with business requirements, ensuring compliance with industry regulations and standards.
+        Consistently completed all tasks ahead of schedule, demonstrating efficiency and dedication to meeting project timelines.
+        AWS Migration for ETL Flow Enhancement:
+
+        Played a key role in the AWS migration project, enhancing the ETL (Extract, Transform, Load) flow for our project.
+        This migration not only improved the efficiency of data processing but also resulted in significant cost and time savings for the organization.
+        EMIR and OTC Projects:
+
+        Took a proactive approach in understanding the functional requirements of the EMIR and OTC projects.
+        Designed and implemented application flows to process data seamlessly from Gemm to Nessie, ensuring data accuracy and compliance.
+        Ensured that the implemented solutions were not only effective for the immediate tasks but also adaptable for future requirements, aligning with a forward-thinking approach.
